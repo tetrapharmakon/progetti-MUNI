765c7f4765e32f3d747903642cb3353dc4e2df35
Fouche <tetrapharmakon@gmail.com>
2017-04-05 19:14:20 +0200

    project in fieri

---
+++ auto/notes.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "notes"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "preambolo"
+    "article"
+    "art12"))
+ :latex)
+
